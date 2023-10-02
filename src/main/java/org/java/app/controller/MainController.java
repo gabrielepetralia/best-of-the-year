b8c9c23358e1e94047452371b5ae34e7039f03bd
gabrielepetralia <gabri.petralia03@gmail.com>
2023-10-02 17:20:48 +0200

    print title at specific index

--- src/main/java/org/java/app/controller/MainController.java
+++ src/main/java/org/java/app/controller/MainController.java
@@ -8,6 +8,8 @@ import org.java.app.pojo.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -52,7 +54,15 @@ public class MainController {
 	            .reduce((s1, s2) -> s1 + ", " + s2)
 	            .orElse("");
 		
+		String indexButtons = "";
+		
+		for(int i=0; i<getBestMovies().size(); i++) {
+			final String button = "<li class='mx-2'><a class='btn btn-danger text-white' href='/movies/" + i + "'>" + (i + 1) + "</a></li>";
+			indexButtons += button;
+		}
+		
 		model.addAttribute("movies", movies);
+		model.addAttribute("indexButtons", indexButtons);
 		
 		return "movies";
 	}
@@ -64,8 +74,56 @@ public class MainController {
 	            .reduce((s1, s2) -> s1 + ", " + s2)
 	            .orElse("");
 		
+		String indexButtons = "";
+		
+		for(int i=0; i<getBestSongs().size(); i++) {
+			final String button = "<li class='mx-2'><a class='btn btn-danger text-white' href='/songs/" + i + "'>" + (i + 1) + "</a></li>";
+			indexButtons += button;
+		}
+		
 		model.addAttribute("songs", songs);
+		model.addAttribute("indexButtons", indexButtons);
 		
 		return "songs";
+	}
+	
+	@GetMapping("/{type}/{id}")
+	public String getMovieSongById(@PathVariable String type, @PathVariable int id, Model model) {
+		final String name = "Gabriele Petralia";
+		model.addAttribute("name", name);
+		
+		if(type.equals("movies")) {
+			
+			List<Movie> movies = getBestMovies();
+			final String title = movies.get(id).getTitle();
+			
+			final String type2 = "movie";
+			
+			model.addAttribute("type", type);
+			model.addAttribute("type2", type2);
+			model.addAttribute("id", id);
+			model.addAttribute("title", title);
+			
+			return "movie-song-by-id";
+			
+		} 
+		
+		if(type.equals("songs")) {
+			
+			List<Song> songs = getBestSongs();
+			final String title = songs.get(id).getTitle();
+			
+			final String type2 = "song";
+			
+			model.addAttribute("type", type);
+			model.addAttribute("type2", type2);
+			model.addAttribute("id", id);
+			model.addAttribute("title", title);
+			
+			return "movie-song-by-id";
+			
+		}
+			
+		return "index";
 	}
 }
